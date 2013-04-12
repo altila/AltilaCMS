ddebde7d6e7c27d1c158b476ebe307af3255a53a
altila <19887258cq>
2013-04-12 17:03:59 +0800

    商品模块优化

--- Modules/AdminProduct/Action/ProductChannelGoodsAction.class.php
+++ Modules/AdminProduct/Action/ProductChannelGoodsAction.class.php
@@ -29,6 +29,7 @@ class ProductChannelGoodsAction extends ABaseAction {
 	public function _before_insert() {
 		if( empty($_REQUEST['smid']) ) $this->ajaxReturn( '', "请选择分类!", 0 );
 		if( empty($_REQUEST['pcid']) ) $this->ajaxReturn( '', "请选择渠道!", 0 );
+		if( empty($_REQUEST['dwz_orgId_pgid']) ) $this->ajaxReturn( '', "请选择商品!", 0 );
 	}
 
 	/**
@@ -39,6 +40,7 @@ class ProductChannelGoodsAction extends ABaseAction {
 	public function _before_update() {
 		if( empty($_REQUEST['smid']) ) $this->ajaxReturn( '', "请选择分类!", 0 );
 		if( empty($_REQUEST['pcid']) ) $this->ajaxReturn( '', "请选择渠道!", 0 );
+		if( empty($_REQUEST['dwz_orgId_pgid']) ) $this->ajaxReturn( '', "请选择商品!", 0 );
 	}
 
 	/**
