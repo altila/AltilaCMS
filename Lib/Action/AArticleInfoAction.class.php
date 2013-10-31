e49966811e727a5a6d4da6afe080dcf44fd08dfa
altila <19887258cq>
2013-10-31 19:29:34 +0800

    单独调用ueditor图片上传功能

--- Lib/Action/AArticleInfoAction.class.php
+++ Lib/Action/AArticleInfoAction.class.php
@@ -18,21 +18,7 @@ class AArticleInfoAction extends ABaseAction {
 	+----------------------------------------------------------
 	*/
 	public function _before_insert() {
-		$description = htmlspecialchars_decode( $_POST['description'] );
-		$content = htmlspecialchars_decode( $_POST['content'] );
-		//文章标签
-		if( !empty($_POST['tags_name']) ) $_POST['tags_name'] = str_replace( '，', ',', $_POST['tags_name'] );
-		//文章简述
-		$_POST['description'] = !empty($description) ? msubstr( strip_tags($description), 0, 100 ) : msubstr( strip_tags($content), 0, 100 );
-		//替换图片路径
-		$uploadPath = C('TMPL_PARSE_STRING');
-		$_POST['content'] = htmlspecialchars( str_replace( $uploadPath['__UPLOAD__'], '__UPLOAD__', html2txt( stripslashes($content),array('script','style') ) ) );
-		//文章内图片
-		preg_match_all('/<\s*img\s+[^>]*?src\s*=\s*(\'|\")(.*?)\\1[^>]*?\/?\s*>/i',stripslashes($content),$match);
-		foreach( $match[2] as $key=>$val ){
-			if( strlen($url) <= 710 ) $url .= "{$val},";
-		}
-		$_POST['img_url'] = str_replace( $uploadPath['__UPLOAD__'], '__UPLOAD__', $url );
+		$this->beforeOperate();
 	}
 
 	/**
@@ -41,6 +27,15 @@ class AArticleInfoAction extends ABaseAction {
 	+----------------------------------------------------------
 	*/
 	public function _before_update() {
+		$this->beforeOperate();
+	}
+
+	/**
+	+----------------------------------------------------------
+	* 前置操作
+	+----------------------------------------------------------
+	*/
+	public function beforeOperate() {
 		$description = htmlspecialchars_decode( $_POST['description'] );
 		$content = htmlspecialchars_decode( $_POST['content'] );
 		//文章标签
@@ -56,6 +51,7 @@ class AArticleInfoAction extends ABaseAction {
 			if( strlen($url) <= 710 ) $url .= "{$val},";
 		}
 		$_POST['img_url'] = str_replace( $uploadPath['__UPLOAD__'], '__UPLOAD__', $url );
+		if( empty($_POST['thumbnail']) ) $_POST['thumbnail'] = str_replace( $uploadPath['__UPLOAD__'], '__UPLOAD__', $match[2][0] );
 	}
 
 }
